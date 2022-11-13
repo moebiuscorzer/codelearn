78accabc4b877f8e68fe4046045a7df73bb6411f
moebiuscorzer <xmauquoy@hotmail.com>
2022-11-13 12:33:13 +0100

    Module 6

--- javascript/pluralsight/javascript-getting-started/demo-03/home.js
+++ javascript/pluralsight/javascript-getting-started/demo-03/home.js
@@ -32,3 +32,55 @@ showMessage(amount); // ignores anything that is not a number or a . or a ,
 
 let saved = true;
 saved = !saved; // saved is now false
+
+let person = {
+  firstName: "John",
+  lastName: "Adams",
+};
+
+showMessage(person.firstName);
+
+/* Module 6 */
+// false
+if (Number.parseFloat("0")) {
+  showMessage("true");
+}
+// true
+if ("0") {
+  showMessage("true");
+}
+// false
+if (1.1 + 1.3 === 2.4) {
+  showMessage("1.1 + 1.3 === 2.4 is true");
+} else {
+  showMessage("1.1 + 1.3 === 2.4 is false");
+}
+/*
+  toFixed returns a string
+  Using '+' before a string makes it a number
+*/
+if (+(1.1 + 1.3).toFixed(2) === 2.4) {
+  showMessage("+(1.1 + 1.3).toFixed(2) === 2.4 is true");
+} else {
+  showMessage("+(1.1 + 1.3).toFixed(2) === 2.4 is false");
+}
+
+showMessage(typeof +"2"); // number
+
+for (let i = 0; i < 3; i++) {
+  console.log("for loop: " + i);
+}
+
+let count = 5;
+while (count > 0) {
+  console.log("while loop: " + count);
+  count--;
+}
+
+console.log("count after while: " + count);
+
+count = 1;
+do {
+  console.log("do while loop: " + count);
+  count++;
+} while (count < 5);

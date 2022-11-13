3feb7bafbe26bc86ac75d30cd03174146348e328
moebiuscorzer <xmauquoy@hotmail.com>
2022-11-13 13:48:56 +0100

    Removed demo-05 folder and modified utils.js as part of module 7

--- javascript/pluralsight/javascript-getting-started/demo-03/utils.js
+++ javascript/pluralsight/javascript-getting-started/demo-03/utils.js
@@ -1,3 +1,15 @@
 function showMessage(message) {
   document.getElementById("message").textContent = message;
 }
+function logMessage(message) {
+  console.log(message);
+}
+/* Function expression */
+let fn = function altLogMessage() {};
+fn(); // correct
+// altLogMessage() is only information for the reader
+function getSecretCode(value) {
+  let code = value * 42;
+  return code;
+}
+console.log(getSecretCode(2));

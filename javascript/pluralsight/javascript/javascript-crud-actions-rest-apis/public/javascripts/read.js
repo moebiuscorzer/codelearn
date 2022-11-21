3221cc54000bf465849bb3553f4895e662738749
moebiuscorzer <xmauquoy@hotmail.com>
2022-11-21 22:04:21 +0100

    Module 2 (end) and Module 3 (ongoing)

--- javascript/pluralsight/javascript/javascript-crud-actions-rest-apis/public/javascripts/read.js
+++ javascript/pluralsight/javascript/javascript-crud-actions-rest-apis/public/javascripts/read.js
@@ -1,25 +1,59 @@
+document.getElementById("load").onclick = function () {
+  /*
+  const req = new XMLHttpRequest();
+  req.open("GET", "/api/products");
+  req.onload = function () {
+    const data = JSON.parse(req.response);
+    addList({ data });
+  };
+
+  req.send();
+  */
+
+  const productId = document.getElementById("product-id").value;
+  if (productId === "") {
+    /* axios is Promise-based
+     This can be seen through the "then" function
+     which takes a callback (function) as input.
+     By default, the result of the GET request
+     performed by axios will be fed as an object
+     argument to the callback (here, "addList")
+  */
+    axios.get("/api/products").then(addList);
+  } else {
+    axios
+      .get(`/api/products/${productId}`)
+      .then(addSingle)
+      .catch((err) => {
+        if (err.response.status === 404) {
+          notFound();
+        }
+      });
+  }
+};
 
 function addList({ data }) {
-  resetContentArea();
+  resetContentArea(); // content area is reset to avoid adding the list infintely
 
-  const template = document.querySelector("#list-result");
+  const template = document.querySelector("#list-result"); // load an HTML template  "#list-result"
   const clone = template.content.cloneNode(true);
 
-  const ul = clone.querySelector("ul");
+  const ul = clone.querySelector("ul"); // initialise a list element
   data.forEach((d) => {
-    const li = clone.querySelector("li").cloneNode(true);
-    const id = li.querySelector("#product-id");
-    id.textContent = d.id;
+    // populate the list
+    const li = clone.querySelector("li").cloneNode(true); // create an list item
+    const id = li.querySelector("#product-id"); // use a template
+    id.textContent = d.id; // set text content
     id.onclick = (e) => {
       document.getElementById("product-id").value = e.currentTarget.textContent;
     };
-    li.querySelector("#product-name").textContent = d.name;
-    li.querySelector("#product-price").textContent = d.price;
+    li.querySelector("#product-name").textContent = d.name; // set text content
+    li.querySelector("#product-price").textContent = d.price; // set text content
 
-    ul.appendChild(li);
+    ul.appendChild(li); // add the list item
   });
 
-  results.appendChild(clone);
+  results.appendChild(clone); // add the cloned item to the document
 }
 
 function addSingle({ data }) {

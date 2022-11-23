1d2de2a53810fccfa12510cee44b600a3617582d
moebiuscorzer <xmauquoy@hotmail.com>
2022-11-23 22:21:27 +0100

    Module 3 end + search start

--- javascript/pluralsight/javascript/javascript-crud-actions-rest-apis/public/javascripts/delete.js
+++ javascript/pluralsight/javascript/javascript-crud-actions-rest-apis/public/javascripts/delete.js
@@ -1,3 +1,11 @@
+document.getElementById("delete").onclick = function (evt) {
+  evt.preventDefault();
+
+  const productId = document.getElementById("product-id").value;
+
+  axios.delete(`/api/products/${productId}`).then(processResult);
+};
+
 function processResult() {
   window.alert("Product deleted!");
 }
--- javascript/pluralsight/javascript/javascript-crud-actions-rest-apis/routes/products.js
+++ javascript/pluralsight/javascript/javascript-crud-actions-rest-apis/routes/products.js
@@ -12,6 +12,16 @@ module.exports = function (db) {
       res.send(db.get("products").insert(newProduct).write());
     });
 
+  router.route("/products/search").get((req, res) => {
+    const keywords = req.query.keywords;
+    const result = db.get("products").filter((_) => {
+      const fullText = _.description + _.name + _.color;
+      return fullText.indexOf(keywords) !== -1;
+    });
+
+    res.send(result);
+  });
+
   router
     .route("/products/:id")
     .get((req, res) => {
